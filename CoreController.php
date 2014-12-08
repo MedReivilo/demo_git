9341d54bffbd69aaf1f91ba9367f807504675492
Olivier <o.pegere@symaris.com>
2014-12-08 20:40:51 +0100

    Creation du fichier CoreController.php de mon projet Symfony2

---
+++ CoreController.php
@@ -0,0 +1,12 @@
+<?php
+
+// src/OC/CoreBundle/Controller/CoreController.php
+
+namespace OC\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CoreController extends Controller
+{
+
+}
